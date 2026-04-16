221b382b0789c87cc6a27da8357f1dc9ce8780c7
mfyx <mfyxyz@gmail.com>
2026-04-16 15:40:06 +0800

    feat: add initial Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
-import type { NextConfig } from 'next';
+// import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   // outputFileTracingRoot: path.resolve(__dirname, '../../'),  // Uncomment and add 'import path from "path"' if needed
   /* config options here */
   allowedDevOrigins: ['*.dev.coze.site'],
